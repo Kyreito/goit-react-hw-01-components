3d8d8c3362cbf507cdf2e3de98122ea46c1b8e7c
Kyreito <m.kyreito@gmail.com>
2021-02-11 16:17:43 +0300

    Started

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const painting = {
+  "id": "id-1",
+  "url": "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+  "title": "Feathers. Art abstract",
+  "price": 500,
+  "author": {
+    "tag": "ractapopulous",
+    "url": "https://pixabay.com/users/ractapopulous-24766/"
+    },
+  "quantity": 10
+  }; 
+
+
+
